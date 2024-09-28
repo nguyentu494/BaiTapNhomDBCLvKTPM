ee5ff1771f7c11dc48774eff58c290a10c4bfb99
nguyentu494 <nguyentu2003.khtgdd@gmail.com>
2024-09-29 03:54:42 +0700

    sln project

---
+++ DHKTPM17C_Vy_Tu/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DHKTPM17C_Vy_Tu")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DHKTPM17C_Vy_Tu")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b15e6ffc-d712-4312-91fd-e2bd6fd1c7bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DHKTPM17C_Vy_Tu/UT_Lab_1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Security.Principal;
+
+namespace DHKTPM17C_Vy_Tu
+{
+    [TestClass]
+    public class UT_Lab_1
+    {
+        public TestContext TestContext { get; set; }
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\UT_Lab_1.csv", "UT_Lab_1#csv", DataAccessMethod.Sequential)]
+        public void TestMethod1()
+        {
+            MethodLibrary.MethodLibrary methodLibrary = new MethodLibrary.MethodLibrary();
+            int inputA = Convert.ToInt32(TestContext.DataRow["a"]);
+            int inputB = Convert.ToInt32(TestContext.DataRow["b"]);
+            int inputC = Convert.ToInt32(TestContext.DataRow["c"]);
+            String expectedRst = Convert.ToString(TestContext.DataRow["rst"]);
+            if (!expectedRst.Equals("exception"))
+            {
+                int expected = int.Parse(expectedRst);
+                int result = methodLibrary.Max(inputA, inputB, inputC);
+                Assert.AreEqual(expected, result);
+            }
+            else
+            {
+                Exception expectedException = null;
+                try
+                {
+                    methodLibrary.Max(inputA,inputB, inputC);
+                }
+                catch (Exception ex)
+                {
+                    // Catches the exception
+                    expectedException = ex;
+                }
+                
+                Assert.IsNotNull(expectedException);
+            }
+
+
+        }
+    }
+}
